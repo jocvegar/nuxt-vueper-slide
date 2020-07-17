a7c3746fb481c7763e53d876970f03f6b8da77e7
Jose <jocvegar@gmail.com>
2020-07-17 14:24:28 -0600

    Adds vueper

---
+++ components/Slides.vue
@@ -0,0 +1,29 @@
+<template>
+    <vueper-slides fade :touchable="false">
+        <vueper-slide
+            v-for="(slide, i) in slides"
+            :key="i"
+            :image="slide.image" />
+    </vueper-slides>
+  
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
+export default {
+    components: { VueperSlides, VueperSlide },
+    data: () => ({
+        slides: [
+            { image: require('@/assets/images/image-1.jpg') },
+            { image: require('@/assets/images/image-2.jpg') },
+            { image: require('@/assets/images/image-3.jpg') },
+        ]
+    })
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,21 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
         vueper-slide
       </h1>
-      <h2 class="subtitle">
-        My first-class Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <slides />
     </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Slides from '~/components/Slides.vue'
 
 export default {
   components: {
-    Logo
+    Logo, Slides
   }
 }
 </script>
@@ -58,15 +40,4 @@ export default {
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
