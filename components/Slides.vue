76379cb9b63a6c671953496d86caeb7209e4d82f
Jose <jocvegar@gmail.com>
2020-07-17 14:38:07 -0600

    Adds details

--- components/Slides.vue
+++ components/Slides.vue
@@ -1,5 +1,21 @@
 <template>
-    <vueper-slides fade :touchable="false">
+    <vueper-slides 
+        autoplay
+        class="no-shadow"
+        :visible-slides="3"
+        slide-multiple
+        :infinite="true" 
+        :arrows="false"
+        :bullets="false"
+        :gap="1"
+        progress
+        :slide-ratio="1 / 3"
+        :dragging-distance="70"
+        :breakpoints="{ 800: { 
+            visibleSlides: 1, 
+            slideRatio: 1,
+            } 
+        }">
         <vueper-slide
             v-for="(slide, i) in slides"
             :key="i"
@@ -19,6 +35,8 @@ export default {
             { image: require('@/assets/images/image-1.jpg') },
             { image: require('@/assets/images/image-2.jpg') },
             { image: require('@/assets/images/image-3.jpg') },
+            { image: require('@/assets/images/image-4.jpg') },
+            { image: require('@/assets/images/image-5.jpg') },
         ]
     })
 }
